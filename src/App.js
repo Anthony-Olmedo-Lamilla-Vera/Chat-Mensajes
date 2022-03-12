f240727d3cc86f9392cf322de2ef00d4aed2c7aa
Lamilla19 <57071153+Lamilla19@users.noreply.github.com>
2022-03-12 10:54:22 -0500

    readme

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginUser from "./Page/LoginUser";
 import Rooms from "./Page/Rooms";
 import Head from "./Components/Head";
 import Sala from "./Page/Sala";
+import PageGit from "./Page/pagegit";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/chat" exact element={<Chat />} />
         <Route path="/Rooms" exact element={<Rooms />} />
         <Route path="/Sala" exact element={<Sala />} />
+        <Route path="/Chat-Mensajes" exact element={<PageGit />} />
       </Routes>
     </div>
   );
---
+++ src/Page/PageGit.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function PageGit() {
+  return (
+    <div>
+      <Link to="/Login">Entrar a Web -- Login</Link>
+    </div>
+  );
+}
+
+export default PageGit;
